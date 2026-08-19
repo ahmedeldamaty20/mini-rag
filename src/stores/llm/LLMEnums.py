06cbb6348933de1f7c0a2cba3b3d5473148b2c35
ahmedeldamaty20 <ahmedeldamaty20@gmail.com>
2026-08-19 13:41:25 +0300

    feat: add Cohere and document type enums

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -9,3 +9,13 @@ class OpenAIEnums(Enum):
   USER = "user"
   ASSISTANT = "assistant"
 
+class CohereEnums(Enum):
+  SYSTEM = "SYSTEM"
+  USER = "USER"
+  ASSISTANT = "CHATBOT"
+  DOCUMENT = "search_document"
+  QUERY = "search_query"
+
+class DocumentTypeEnums(Enum):
+  DOCUMENT = "document"
+  QUERY = "query"
