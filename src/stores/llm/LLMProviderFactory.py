fbf69dafa43a34aa865b274101c4aa800c2e1e5a
ahmedeldamaty20 <ahmedeldamaty20@gmail.com>
2026-08-19 15:11:11 +0300

    feat: implement LLMProviderFactory to create OpenAI/Cohere providers

---
+++ src/stores/llm/LLMProviderFactory.py
@@ -0,0 +1,28 @@
+from helpers.config import settings
+from .LLMEnums import LLMEnums
+from .providers import OpenAIProvider, CohereProvider
+
+
+class LLMProviderFactory:
+
+  def __init__(self, config: settings):
+    self.config = config
+
+  def get_provider(self, provider_name: str):
+    if provider_name == LLMEnums.OPENAI.value:
+      return OpenAIProvider(
+        api_key=self.config.OPENAI_API_KEY,
+        default_input_max_chars=self.config.INPUT_DEFAULT_MAX_CHARACTERS,
+        default_generation_max_output_tokens=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+        default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
+      )
+    elif provider_name == LLMEnums.COHERE.value:
+      return CohereProvider(
+        api_key=self.config.OPENAI_API_KEY,
+        default_input_max_chars=self.config.INPUT_DEFAULT_MAX_CHARACTERS,
+        default_generation_max_output_tokens=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+        default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
+      )
+
+    return None
+       
