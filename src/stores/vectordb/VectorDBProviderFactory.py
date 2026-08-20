a8178425553d4f5b710562071c1d4046db4d97b7
ahmedeldamaty20 <ahmedeldamaty20@gmail.com>
2026-08-20 12:26:54 +0300

    feat: add VectorDBProviderFactory to instantiate providers by backend name

---
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -0,0 +1,22 @@
+from .providers import QdrantDBProvider
+from helpers.config import settings
+from .VectorDBEnums import VectorDBEnums
+from controllers.BaseController import BaseController
+
+class VectorDBProviderFactory:
+
+  def __init__(self, config: settings):
+      self.config = config
+      self.base_controller = BaseController()
+
+  def get_provider(self, provider_name: str):
+    if provider_name == VectorDBEnums.QDRANT.value:
+      # Ensure the database path exists
+      db_path = self.base_controller.get_database_path(self.config.QDRANT_DB_PATH)
+      return QdrantDBProvider(
+        db_path=db_path,
+        distance_method=self.config.QDRANT_DISTANCE_METHOD
+      )
+
+    return None
+
