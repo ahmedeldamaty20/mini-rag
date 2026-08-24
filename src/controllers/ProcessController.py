2179c4c370dc83b6fdac597d35fb3b13ac21c110
ahmedeldamaty20 <ahmedeldamaty20@gmail.com>
2026-08-24 17:58:14 +0300

    refactor: improve chunking strategy

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -1,11 +1,16 @@
-from typing import Optional
+from typing import Any, List, Optional
 from .BaseController import BaseController
 from .ProjectController import ProjectController
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dataclasses import dataclass
 from models import ProcessingEnum
 import os
+
+@dataclass
+class Document:
+  texts: list[str]
+  metadatas: Optional[list[dict[Any, Any]]] = None
 
 class ProcessController(BaseController):
   def __init__(self, project_id: int):
@@ -39,12 +44,7 @@ class ProcessController(BaseController):
     return documents
 
   def process_file_content(self, file_content: list, file_id: str, chunk_size: Optional[int] = 1000, overlap_size: Optional[int] = 20):
-    text_splitter = RecursiveCharacterTextSplitter(
-      chunk_size = chunk_size,
-      chunk_overlap = overlap_size,
-      length_function = len
-    )
-  
+
     file_content_texts = [
       rec.page_content
       for rec in file_content
@@ -55,10 +55,35 @@ class ProcessController(BaseController):
       for rec in file_content
     ]
 
-    chunks = text_splitter.create_documents(
+    chunks = self.process_simpler_splitter(
       file_content_texts,
-      metadatas = file_content_metadata
+      file_content_metadata,
+      chunk_size=chunk_size if chunk_size else 1000
     )
 
     return chunks
 
+  def process_simpler_splitter(self, file_content: List[str], metadatas: List[dict], chunk_size: int = 1000, splitter: str = "\n"):
+    full_text = " ".join(file_content)
+
+    lines = [chunk.strip() for chunk in full_text.split(splitter) if len(chunk.strip()) > 1 ]
+
+    chunks: list[Document] = []
+    current_chunk = ""
+
+    for line in lines:
+      current_chunk += line + splitter
+      if len(current_chunk) >= chunk_size:
+        chunks.append(Document(
+          texts=[current_chunk.strip()],
+          metadatas=metadatas
+        ))
+        current_chunk = ""
+
+    if current_chunk:
+      chunks.append(Document(
+        texts=[current_chunk.strip()],
+        metadatas=metadatas
+      ))
+
+    return chunks
